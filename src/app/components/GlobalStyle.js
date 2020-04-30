f866f40ad63bdecb14cd7532aa12278d0c411ed5
DevSp <contatodevsp@gmail.com>
2020-04-30 19:58:51 -0300

    adicionando fonts

--- src/app/components/GlobalStyle.js
+++ src/app/components/GlobalStyle.js
@@ -6,6 +6,7 @@ const GlobalStyle = createGlobalStyle`
         box-sizing: border-box;
         margin: 0;
         padding: 0;
+        font-family: 'Roboto', sans-serif;
     }
     
     html, body, #root { 
