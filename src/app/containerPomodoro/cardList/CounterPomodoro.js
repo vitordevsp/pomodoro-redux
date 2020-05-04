5443c3223f7580b8ae0a29870010eb7da51da5cd
DevSp <contatodevsp@gmail.com>
2020-05-03 23:28:01 -0300

    mostrando a contagem no comp CounterPomodoro

--- src/app/containerPomodoro/cardList/CounterPomodoro.js
+++ src/app/containerPomodoro/cardList/CounterPomodoro.js
@@ -3,11 +3,16 @@ import Horizontal from '../../components/Horizontal'
 import Text from '../../components/Text'
 
 
-function CounterPomodoro() {
+function CounterPomodoro({ array }) {
+
+    const countPomodoro = array.length
+    let countPomodoroCompleted = 0
+    array.forEach(obj => obj.completed ? ++countPomodoroCompleted : false)
+
     return (
         <Horizontal margin='20px' justify='space-between'>
             <Text>Objetivo diario: </Text>
-            <Text>0 - 3</Text>
+            <Text>{`${countPomodoroCompleted} - ${countPomodoro}`}</Text>
         </Horizontal>
     )
 }
