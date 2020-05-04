4c006bb6bb2691a491a656ad3f18673dc71e4218
DevSp <contatodevsp@gmail.com>
2020-05-03 22:39:20 -0300

    add novo pomodoro

--- src/app/containerPomodoro/cardList/index.js
+++ src/app/containerPomodoro/cardList/index.js
@@ -3,23 +3,32 @@ import Card from '../../components/Card'
 import CounterPomodoro from './CounterPomodoro'
 import InputAdd from '../../components/InputAdd'
 import ListPomodoros from './ListPomodoros'
+import { useDispatch, useSelector } from 'react-redux'
+import { pomodoro } from '../../../store/actions'
 
 
 function CardList() {
+
+    const dispatch = useDispatch()
+    const arrayPomodoros = useSelector(state => state.pomodoros)
 
     const [newPomodoro, setNewPomodoro] = useState('')
 
     const onChange = event => setNewPomodoro(event.target.value)
 
     const onClickAdd = () => {
-        console.log(newPomodoro)
+        if (newPomodoro !== '') {
+            dispatch(pomodoro.add(`#${arrayPomodoros.length + 1}`, newPomodoro))
+            setNewPomodoro('')
+            document.getElementById('inputPomodoro').focus()
+        }
     }
 
     return (
         <Card>
-            <CounterPomodoro />
-            <InputAdd placeholder='Novo Pomodoro: ' value={newPomodoro} onChange={onChange} onClickAdd={onClickAdd} margin='0 24px 18px' />
-            <ListPomodoros />
+            <CounterPomodoro array={arrayPomodoros} />
+            <InputAdd id='inputPomodoro' placeholder='Novo Pomodoro: ' value={newPomodoro} onChange={onChange} onClickAdd={onClickAdd} margin='0 24px 18px' />
+            <ListPomodoros array={arrayPomodoros} />
         </Card>
     )
 }
