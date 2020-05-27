6a13263dcd0cec67de4e4e75f208f58040f7e9c1
DevSp <contatodevsp@gmail.com>
2020-05-27 09:26:08 -0300

    Updates

---
+++ precache-manifest.cd00eac20fe0babdccb8c6a7f57a5753.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf62b96c52fe77d3e67430098677640a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d34142cebdded5efa742",
+    "url": "/static/js/2.68ebbdd4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.68ebbdd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58ad81a04365b3c07305",
+    "url": "/static/js/main.110d89f8.chunk.js"
+  },
+  {
+    "revision": "e0778092f5b1c26b4a0a",
+    "url": "/static/js/runtime-main.800ecef1.js"
+  }
+]);
