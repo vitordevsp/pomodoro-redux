b410eeadc5de389a35f4fd5ef78fbe46da75b11d
DevSp <contatodevsp@gmail.com>
2020-05-26 22:41:07 -0300

    separando state

---
+++ src/store/state.js
@@ -0,0 +1,20 @@
+const INITIAL_STATE = {
+    pomodoros: [
+        {
+            indicator: '#1',
+            name: 'Primeiro pomodoro',
+            selected: true,
+            completed: false,
+            time: false,
+            toDoList: [
+                {
+                    done: false,
+                    indicator: '#1',
+                    name: 'Primeira tarefa',
+                }
+            ]
+        }
+    ]
+}
+
+export default INITIAL_STATE
